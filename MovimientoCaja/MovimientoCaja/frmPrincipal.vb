edef5a18f5f03cf414920f8bf7356ee92927383a
Alex Stroe <alexstroe91@gmail.com>
2021-11-17 18:22:21 +0100

    Ajusto la posicion de la imagen a la mitad

--- MovimientoCaja/MovimientoCaja/frmPrincipal.vb
+++ MovimientoCaja/MovimientoCaja/frmPrincipal.vb
@@ -1,4 +1,7 @@
 ﻿Public Class frmPrincipal
     Private Sub frmPrincipal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'ajusto la posicion de la caja a la mitad
+        lblImagen.Top = Me.Height / 6
+        lblImagen.Left = Me.Width / 2.25
     End Sub
 End Class
